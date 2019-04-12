461d8c4f6d9ad6d12bb77263ac1b48e3aaba46f4
Zsolt Gomori <30864202+gomorizsolt@users.noreply.github.com>
2019-04-12 11:25:34 +0200

    Auto commit/push setup (#3)
    * Extend .travis.yml with distify stage
    
    * Execute distify.sh with bash, remove lint/test stages
    
    * Refine distify.sh and .travis.yml
    
    * Add commit-dist.sh, experiment git remove -v
    
    * Swap HTTPS connection with SSH in commit-dist.sh
    
    * Add the encrypted key to the scripts folder
    
    * Extend commit-dist.sh with further git commands
    
    * Add commit command to commit-dish.sh
    
    * Investigate git commands
    
    * Add git pull --all command to commit-dist.sh
    
    * Add branch restriction to .travis.yml, restructure commit/push flow in commit-dist.sh
    
    * Rename dist-commit.sh, determine current branch in .travis.yml
    
    * Fix bash path in .travis.yml, remove lint/test stages
    
    * Add before_install to .travis.yml
    
    * Print  in .travis.yml
    
    * Change echo command in .travis.yml
    
    * Add distify branch restriction to .travis.yml
    
    * Rename states to stages in .travis.yml
    
    * Add distify folder
    
    * Re-add lint stage
    
    * Remove distify stage branch restriction
    
    * Re-add distify stage branch restriction
    
    * Remove distify folder
    
    * Remove quotes around the strings
    
    * Add quotes around master in .travis.yml
    
    * Re-add branch restriction to .travis.yml
    
    * Restructure distify.sh
    
    * Remove build stage from .travis.yml
    
    * Re-add lint and test stages to .travis.yml
    
    * Reformat stage restriction
    
    * [skip travis] Test CI skipping keyword
    
    * Add [skip travis] to the commit message
    
    * Add skip travis to the commit message
    
    * Add master branch build restriction to .travis.yml
    
    * Remove push command from distify.sh
    
    * Remove master branch deployment restricton from .travis.yml
    
    * Re-add git push command to distify.sh

--- scripts/distify.sh
+++ scripts/distify.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-NODE_MODULES_BIN_PATH=`pwd`/node_modules/.bin
+generate_distified_file() {
+    NODE_MODULES_BIN_PATH=`pwd`/node_modules/.bin
 
-rm -r dist
-mkdir -p dist
+    mkdir dist
 
-$NODE_MODULES_BIN_PATH/babel lib/index.js -o dist/index.min.js
+    $NODE_MODULES_BIN_PATH/babel lib/index.js -o dist/index.min.js
 
-$NODE_MODULES_BIN_PATH/browserify dist/index.min.js -o dist/index.min.js
+    $NODE_MODULES_BIN_PATH/browserify dist/index.min.js -o dist/index.min.js
 
-$NODE_MODULES_BIN_PATH/uglifyjs dist/index.min.js -o dist/index.min.js
+    $NODE_MODULES_BIN_PATH/uglifyjs dist/index.min.js -o dist/index.min.js
+
+    rm -r lib
+}
+
+upload_minified_version() {
+    eval "$(ssh-agent -s)"
+    openssl aes-256-cbc -K $encrypted_d9591a2445c1_key -iv $encrypted_d9591a2445c1_iv -in ./scripts/id_rsa_ci.enc -out id_rsa_ci -d
+    chmod 600 id_rsa_ci
+    ssh-add id_rsa_ci
+
+    git add dist/*
+    git commit -m "[skip travis] Update distified version"
+    git push --set-upstream origin master 
+}
+
+generate_distified_file
+upload_minified_version
