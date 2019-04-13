7119b610ec64bd66c56815092b0565cba2cf3627
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-13 09:42:58 +0200

    Refine proxy url getters

--- src/utils/Proxy/Proxy.js
+++ src/utils/Proxy/Proxy.js
@@ -1,5 +1,5 @@
 export const defaultProxyServerUrl = 'https://c-hive-proxy.herokuapp.com/';
 
-export const getGitHubProxyUrl = gitHubUsername => `https://c-hive-proxy.herokuapp.com/https://github.com/users/${gitHubUsername}/contributions`;
+export const getGitHubProxyUrl = (proxyServerUrl, gitHubUsername) => `${proxyServerUrl}https://github.com/users/${gitHubUsername}/contributions`;
 
-export const getGitLabProxyUrl = gitLabUsername => `https://c-hive-proxy.herokuapp.com/https://gitlab.com/users/${gitLabUsername}/calendar.json`;
+export const getGitLabProxyUrl = (proxyServerUrl, gitLabUsername) => `${proxyServerUrl}https://gitlab.com/users/${gitLabUsername}/calendar.json`;
--- src/utils/Proxy/Proxy.test.js
+++ src/utils/Proxy/Proxy.test.js
@@ -2,22 +2,25 @@ import { expect } from 'chai';
 import * as Proxy from './Proxy';
 
 describe('Proxy', () => {
+  const proxyServerUrl = 'https://cors-proxy.com';
+
   describe('getGitHubProxyUrl', () => {
     it('returns the `proxied` GitHub contributions URL', () => {
       const gitHubUsername = 'testGitHubUsername';
-      const expectedUrl = `https://c-hive-proxy.herokuapp.com/https://github.com/users/${gitHubUsername}/contributions`;
+      const expectedUrl = `${proxyServerUrl}https://github.com/users/${gitHubUsername}/contributions`;
 
-      const actualUrl = Proxy.getGitHubProxyUrl(gitHubUsername);
+      const actualUrl = Proxy.getGitHubProxyUrl(proxyServerUrl, gitHubUsername);
 
       expect(actualUrl).to.equal(expectedUrl);
     });
   });
+
   describe('getGitLabProxyUrl', () => {
     it('returns the `proxied` GitLab JSON calendar URL', () => {
       const gitLabUsername = 'testGitLabUsername';
-      const expectedUrl = `https://c-hive-proxy.herokuapp.com/https://gitlab.com/users/${gitLabUsername}/calendar.json`;
+      const expectedUrl = `${proxyServerUrl}https://gitlab.com/users/${gitLabUsername}/calendar.json`;
 
-      const actualUrl = Proxy.getGitLabProxyUrl(gitLabUsername);
+      const actualUrl = Proxy.getGitLabProxyUrl(proxyServerUrl, gitLabUsername);
 
       expect(actualUrl).to.equal(expectedUrl);
     });
