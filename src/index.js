6e8bc3eeced94be372446fb9d58221b7c3122386
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-13 22:42:22 +0200

    Rename processData to processParams in Main.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const TeamContributionCalendar = (
   proxyUrl = Proxy.defaultProxyServerUrl,
 ) => {
   if (CalendarUtils.RequiredParamsExist(container, gitHubUsers)) {
-    Main.processData(container, gitHubUsers, gitLabUsers, proxyUrl);
+    Main.processParams(container, gitHubUsers, gitLabUsers, proxyUrl);
   } else {
     throw new Error('Please provide the required parameters in the appropriate format.');
   }
