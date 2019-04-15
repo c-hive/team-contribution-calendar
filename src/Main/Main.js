db83b06b89b5ff1ad3cc23180c6953c74c1c4726
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-15 12:27:13 +0200

    Remove unused variables from Main.js

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,6 @@
 import * as CalendarUtils from '../utils/CalendarUtils/CalendarUtils';
 import BasicCalendar from '../resources/BasicCalendar/BasicCalendar.json';
 
-// eslint-disable-next-line no-unused-vars
-export const processParams = (container, gitHubUsers, gitLabUsers, proxyUrl) => {
+export const processParams = (container) => {
   CalendarUtils.RenderCalendarWithContributions(container, BasicCalendar, 0);
 };
