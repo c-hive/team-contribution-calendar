f9cf67071ba3713871befa1a58778e11e7971808
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-15 14:25:03 +0200

    Remove comments from index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import * as Main from './Main/Main';
 import * as CalendarUtils from './utils/CalendarUtils/CalendarUtils';
 import * as Proxy from './utils/Proxy/Proxy';
 
-// https://github.com/babel/babel/issues/9819
-// https://github.com/babel/gulp-babel/issues/50#issuecomment-255606255
-
 const TeamContributionCalendar = (
   container,
   gitHubUsers,
